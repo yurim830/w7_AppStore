0eca12f7bd7ea8c6d9c7e44767f0f62c161cb677
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-04-11 19:41:29 +0900

    ViewController 생성 및 연결

---
+++ w7_AppStore/AppStoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AppStoreTableViewCell.swift
+//  w7_AppStore
+//
+//  Created by 유림 on 4/11/24.
+//
+
+import UIKit
+
+class AppStoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ w7_AppStore/AppStoreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreViewController.swift
+//  w7_AppStore
+//
+//  Created by 유림 on 4/11/24.
+//
+
+import UIKit
+
+class AppStoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
